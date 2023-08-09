70be89c88a5e0dc57f85ae3797db2424518c6956
Lgelvez <deluargel@gmail.com>
2023-08-09 09:10:28 -0500

    Cuarta entrada: factorizacion

--- script-1.py
+++ script-1.py
@@ -13,3 +13,20 @@ def resta(a,b):
 # multiplicacion
 def multiplicacion(a,b):
     return a * b
+
+# factorización 
+
+def factorizacion(n):
+    factors = []
+    while n % 2 == 0:
+        factors.append(2)
+        n //= 2
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        while n % i == 0:
+            factors.append(i)
+            n //= i
+    if n > 2:
+        factors.append(n)
+    return factors
+
+
